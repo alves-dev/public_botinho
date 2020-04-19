88641df0204f80027a708a84db41ad9e7c40e49a
Igor Moreira <alvesmoreiraigor@gmail.com>
2020-04-19 07:18:58 -0300

    Initial

--- core/__init__.py
+++
@@ -1,5 +0,0 @@
-from core.constants import APPLICATION_SERVER
-
-import core.telegram_commands as tc
-
-tc.set_webhook(APPLICATION_SERVER)
--- core/views.py
+++
@@ -1,9 +0,0 @@
-from django.views.decorators.csrf import csrf_exempt
-import core.utils as u
-from django.http.response import HttpResponse
-
-
-@csrf_exempt
-def event(requests):
-    u.trata_conversa(requests)
-    return HttpResponse()
